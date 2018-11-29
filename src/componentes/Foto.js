ca83ed3b13fc0c4b3f62f0b0b53ef30084e7d6d2
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2018-11-29 15:35:04 -0200

    add changes course

---
+++ src/componentes/Foto.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+
+class FotoAtualizacoes extends Component {
+    render(){
+        return (
+            <section className="fotoAtualizacoes">
+              <a href="#" className="fotoAtualizacoes-like">Likar</a>
+              <form className="fotoAtualizacoes-form">
+                <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo"/>
+                <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
+              </form>
+
+            </section>            
+        );
+    }
+}
+
+class FotoInfo extends Component {
+    render(){
+        return (
+            <div className="foto-in fo">
+              <div className="foto-info-likes">
+                {
+                  this.props.foto.likers.map(liker => {
+                    return (<a href="">{liker.login},</a>)
+                  })
+                }
+                
+                curtiram
+              </div>
+
+              <p className="foto-info-legenda">
+                <a className="foto-info-autor">autor </a>
+                {this.props.foto.comentario}
+              </p>
+
+              <ul className="foto-info-comentarios">
+              {
+                this.props.foto.comentarios.map(comentario => {
+                  return <li className="comentario">
+                          <a className="foto-info-autor">{comentario.login} </a>
+                          {comentario.texto}
+                        </li>
+                })
+              }
+                
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
+                </li>
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
+                </li>
+              </ul>
+            </div>            
+        );
+    }
+}
+
+class FotoHeader extends Component {
+    render(){
+        return (
+            <header className="foto-header">
+              <figure className="foto-usuario">
+                <img src={this.props.foto.urlPerfil} alt="foto do usuario"/>
+                <figcaption className="foto-usuario">
+                  <a href="#">
+                  {this.props.foto.loginUsuario}
+                  </a>  
+                </figcaption>
+              </figure>
+              <time className="foto-data">{this.props.foto.horario}</time>
+            </header>
+        );
+    }
+}
+
+export default class Foto extends Component {
+    render(){
+        return (
+          <div className="foto">
+            <FotoHeader foto={this.props.foto}/>
+            <img alt="foto" className="foto-src" src={this.props.urlFoto}/>
+            <FotoInfo foto={this.props.foto}/>
+            <FotoAtualizacoes/>
+          </div>            
+        );
+    }
+}
---
+++ src/componentes/Timeline.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Foto from './Foto';
+
+export default class Timeline extends Component {
+
+    constructor(){
+        super();
+        this.state = {fotos:[]}
+    }
+
+    componentDidMount(){
+        fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
+            .then(response => response.json())
+            .then(fotos => {
+                this.setState({fotos})
+            })
+    }
+    render(){
+        return (
+        <div className="fotos container">
+          {
+              this.state.fotos.map(foto => <Foto foto={foto} />)
+          }
+        </div>            
+        );
+    }
+}
